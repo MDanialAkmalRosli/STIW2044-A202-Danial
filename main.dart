590b2e38de6b996b1c1628272bf268987697277f
M. D. Akmal <61193939+MDanialAkmalRosli@users.noreply.github.com>
2021-03-30 00:01:52 +0800

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,110 +1,214 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Controller regarding age
+  TextEditingController ageSpace = new TextEditingController();
+
+  // Controller regarding height
+  TextEditingController heightSpace = new TextEditingController();
+
+  // Controller regarding weight
+  TextEditingController weightSpace = new TextEditingController();
+
+  // Controller regarding waist circumference
+  TextEditingController waistCSpace = new TextEditingController();
+
+  double height;
+  double weight;
+  double waist;
+  double result = 0.0;
+  int _value = 1;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      title: 'Material App',
+      home: Scaffold(
+        backgroundColor: Colors.yellow[200],
+        appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text('ABSI Measurement Calculator')),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(20.0),
+          child: Container(
+            height: 500,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Enter your information as shown below."),
+                    Container(
+                      height: 50,
+                    ),
+                  ],
+                ),
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text("Sex"),
+                    Container(
+                      height: 40,
+                      width: 100,
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+                      child: DropdownButton(
+                          value: _value,
+                          items: [
+                            DropdownMenuItem(
+                              child: Text("Male"),
+                              value: 1,
+                            ),
+                            DropdownMenuItem(
+                              child: Text("Female"),
+                              value: 2,
+                            ),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              _value = value;
+                            });
+                          }),
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+                    )
+                  ],
+                ),
 
-  final String title;
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Age"),
+                    Container(
+                      height: 40,
+                      width: 100,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        controller: ageSpace,
+                      ),
+                    ),
+                    Text("years"),
+                  ],
+                ),
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Height"),
+                    Container(
+                      height: 40,
+                      width: 100,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        controller: heightSpace,
+                      ),
+                    ),
+                    Text("cm"),
+                  ],
+                ),
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Weight"),
+                    Container(
+                      height: 40,
+                      width: 100,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        controller: weightSpace,
+                      ),
+                    ),
+                    Text("kg"),
+                  ],
+                ),
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Waist circum."),
+                    Container(
+                      height: 40,
+                      width: 100,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        controller: waistCSpace,
+                      ),
+                    ),
+                    Text("cm"),
+                  ],
+                ),
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+                SizedBox(
+                  height: 10,
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RaisedButton(
+                      child: Text("Get result"),
+                      onPressed: () => calculateRes(1),
+                      color: Color(0xffFF1744),
+                      textColor: Colors.white,
+                    ),
+                  ],
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text("RESULTS"),
+                    Container(
+                      height: 40,
+                      width: 100,
+                    )
+                  ],
+                ),
+                
+                Text("ABSI: \t\t\t\t\t" + result.toStringAsFixed(5)),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void calculateRes(double bmi) {
+    setState(() {
+      height = double.parse(heightSpace.text) / 100;
+      weight = double.parse(weightSpace.text);
+      waist = double.parse(waistCSpace.text) / 100;
+      double bmi = weight / (height * height);
+
+      double absi = waist / (pow(bmi, 2.0 / 3.0) * pow(height, 1.0 / 2.0));
+      result = absi;
+      print(result.toStringAsFixed(5));
+
+      if(absi < -0.868){
+        print("You have very low mortality risk.");
+      }
+      else if(absi >= -0.868 && absi < -0.272){
+        print("You have low mortality risk.");
+      }
+      else if(absi >= -0.272 && absi < 0.229){
+        print("You have average mortality risk.");
+      }
+      else if(absi >= 0.229 && absi < 0.798){
+        print("You have high mortality risk.");
+      }
+      else{
+        print("You have very high mortality risk.");
+      }
+
+    });
   }
 }
